3ce8881c9d98064726a10f9a644ff2e208f66696
hiudyy <hiudyyy@gmail.com>
2025-11-02 15:11:07 -0300

    fix: adicionar nova linha no final do arquivo e corrigir a exportação de funções

--- dados/src/funcs/private/temuScammer.js
+++ dados/src/funcs/private/temuScammer.js
@@ -17,3 +17,8 @@ function convertTemuLink(link) {
     let newLink = "https://www.temu.com/br/bmw.html?subj=downloadable-ads-shopping&tmpl=dn9&_x_ads_sub_channel=shopping&_p_rfs=1&_x_ns_prz_type=-1&_x_ns_sku_id={ID}&goods_id={ID}&sku_id={ID}&_x_gmc_account=5362938519&_x_login_type=Google&_p_jump_id=962&adg_ctx=a-a7937f52~c-df9607e9&locale_override=29~pt~BRL&_x_ns_gid={ID}&mrk_rec=1&_x_ads_channel=google&_bg_fs=1&_x_vst_scene=adg".replaceAll(/{ID}/g, productId);
     return newLink;
 }
+
+module.exports = {
+    extractProductId,
+    convertTemuLink
+};
